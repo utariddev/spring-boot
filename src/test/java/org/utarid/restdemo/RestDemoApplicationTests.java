c9363f506087ce91f39307856c02538f407b9257
utarid <utarid@protonmail.com>
2023-04-25 00:10:19 +0200

    initial commit

---
+++ src/test/java/org/utarid/restdemo/RestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.utarid.restdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
